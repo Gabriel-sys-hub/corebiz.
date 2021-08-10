20d9f1aa196c8b3c7f53cdf153fe98b10d944dad
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-08-10 17:36:27 -0300

    New Build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { HomePage } from './Pages/HomePage';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-    </Switch>
+      <HomePage />
   );
 }
 
